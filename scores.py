3c2708dcae1e85d2fbf2684d07cb9641f285be6d
Kozsof <97364550+Kozsof@users.noreply.github.com>
2022-01-17 01:14:16 +0300

    Update scores.py

--- scores.py
+++ scores.py
@@ -60,7 +60,7 @@ class Life(Sprite):
     def __init__(self, screen):
         super(Life, self).__init__()
         self.screen = screen
-        self.image = pygame.image.load("images/heart.png")
+        self.image = pygame.image.load("heart.png")
         self.rect = self.image.get_rect()
         self.screen_rect = self.screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
